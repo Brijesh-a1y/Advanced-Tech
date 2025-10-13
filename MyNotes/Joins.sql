5c0ac80414eb975a5c0dcb11b7ec09eaab41bbef
Brijesh-a1y <Brijesh.verma@esyasoft.com>
2025-10-13 16:15:31 +0530

    Joins

--- MyNotes/Joins.sql
+++ MyNotes/Joins.sql
@@ -2,11 +2,79 @@ Joins =>
 A JOIN clause is used to combine rows from two or more tables, based on a related column between them.
 Types of Joins:
 1. INNER JOIN: Returns records that have matching values in both tables.
-2. LEFT JOIN (or LEFT OUTER JOIN): Returns all records from the left table, and the matched records from the right table. If there is no match, NULL values are returned for columns from the right table.
+
+2. LEFT JOIN (or LEFT OUTER JOIN): Returns all records from the left table, and the matched records from the right table. 
+    If there is no match, NULL values are returned for columns from the right table.
+
 3. RIGHT JOIN (or RIGHT OUTER JOIN): Returns all records from the right table,
     and the matched records from the left table. If there is no match, NULL values are returned for columns from the left table.
-4. FULL JOIN (or FULL OUTER JOIN): Returns all records when there is a match in either left or right table. If there is no match, NULL values are returned for columns from the table without a match.  
+
+4. FULL JOIN (or FULL OUTER JOIN): Returns all records when there is a match in either left or right table. If there is no match,
+     NULL values are returned for columns from the table without a match.  
+Tables:
+employees table
+employee_id	employee_name	department_id
+1	            Alice	    101
+2	            Bob	        102
+3	            Charlie	    103
+4	            David	    NULL
+
+departments table
+department_id	department_name
+101	            HR
+102	            Finance
+104	            Marketing
+
+Goal: Retrieve a list of all employees and their departments, including employees with no department and departments with no
+employees. 
+SELECT
+  e.employee_name,
+  d.department_name
+FROM
+  employees AS e
+FULL OUTER JOIN
+  departments AS d
+ON
+  e.department_id = d.department_id;
+
+Result of the query
+The output will show all employees and all departments, combining the matched rows and including unmatched rows with NULL values. 
+employee_name	department_name
+Alice	        HR
+Bob	            Finance
+Charlie	        NULL
+David	        NULL
+NULL	        Marketing
+
+
+
 5. CROSS JOIN: Returns the Cartesian product of the two tables, i.e., all possible combinations of rows from both tables.
+
+Imagine two tables:
+
+products
+product_id	product_name
+1	        T-Shirt
+2	        Jeans
+
+colors
+color_id	color_name
+1	        Red
+2	        Blue
+
+SELECT p.product_name,c.color_name
+FROM products p
+CROSS JOIN
+colors c;
+
+Result:
+product_name	color_name
+T-Shirt 	    Red
+T-Shirt	        Blue
+Jeans	        Red
+Jeans	        Blue
+
+
 6. SELF JOIN: A self join is a regular join, but the table is joined with itself.
 Example: The Employees table
 Consider a table named Employees that contains both employees and their managers. 
@@ -15,10 +83,28 @@ EmployeeID	EmployeeName	ManagerID
 102	        Jane Doe	101
 103	        Peter Jones	101
 104	        Alice Chen	102
-Note: The ManagerID column refers to the EmployeeID of that employee's' manager. A NULL value indicates that the employee (in this case, John Smith) has no manager and is at the top of the hierarchy.
+Note: The ManagerID column refers to the EmployeeID of that employee's' manager. A NULL value indicates that the employee 
+(in this case, John Smith) has no manager and is at the top of the hierarchy.
+
+SELECT
+    e.EmployeeName AS Employee,
+    m.EmployeeName AS Manager
+FROM
+    Employees AS e
+LEFT JOIN
+    Employees AS m ON e.ManagerID = m.EmployeeID;
 
 
-7. NATURAL JOIN: A natural join is based on all columns in the two tables that have the same name and selects rows with equal values in the relevant columns.
+Output-
+Employee	Manager
+John Smith	NULL
+Jane Doe	John Smith
+Peter Jones	John Smith
+Alice Chen	Jane Doe
+
+
+7. NATURAL JOIN: A natural join is based on all columns in the two tables that have the same name and selects rows with 
+equal values in the relevant columns.
 example: SELECT * FROM table1 NATURAL JOIN table2;
 Notes = Support only some DBMS (e.g., MySQL, PostgreSQL, Oracle). Not supported in SQL Server.
 Employee table
