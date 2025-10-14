72a890777112591160a3942db1196f111dea908a
Brijesh-a1y <Brijesh.verma@esyasoft.com>
2025-10-14 17:27:50 +0530

    rest api

---
+++ dotNet/Day-05/Class.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Class : ControllerBase
+    {
+        [HttpPost]
+        public string postmethod([FromBody]string name,int age)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            return name;
+        }
+    }
+}
