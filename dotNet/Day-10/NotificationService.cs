eec345d11293115be46ac2abdaa905a34d920b11
Brijesh-a1y <Brijesh.verma@esyasoft.com>
2025-10-23 16:54:44 +0530

    Inversion of control IOC

---
+++ dotNet/Day-10/NotificationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOC
+{
+    internal class NotificationService
+    {
+        private readonly IMessageService _messageService;
+
+        // Dependency is injected through constructor
+        public NotificationService(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        public void Notify(string message)
+        {
+            _messageService.SendMessage(message);
+        }
+    }
+}
---
+++ dotNet/Day-10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO.Pipes;
+
+namespace IOC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            NotificationService ns = new NotificationService(new EmailService());
+            ns.Notify("Email service"); 
+        }
+    }
+}
